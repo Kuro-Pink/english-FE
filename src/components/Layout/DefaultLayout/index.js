5653c2af1ce50c0d58de7f2195d6cece7eff5f8b
Nguyen Diep Hong Hieu <nguyendiephonghieu04072004gmail.com>
2023-11-30 23:48:49 +0700

    built layout

---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,16 @@
+import Header from '~/components/Layout/components/Header';
+import Sitebar from './Sitebar';
+
+function DefaultLayout({ children }) {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <Sitebar />
+                <div className="content">{children}</div>
+            </div>
+        </div>
+    );
+}
+
+export default DefaultLayout;
